5341dc3980732a9de2bc57799fbc553ade5d09be
MrSanZz <95021841+MrSanZz@users.noreply.github.com>
2025-02-04 21:53:43 +0700

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,7 @@
+import os
+
+os.system('pip3 install pycryptodome')
+os.system('pip3 install argparse')
+os.system('pip3 install colorama')
+os.system('pip3 install keyboard' if os.name == 'nt' else 'pip3 install pynput')
+os.system('pip3 install psutil')
